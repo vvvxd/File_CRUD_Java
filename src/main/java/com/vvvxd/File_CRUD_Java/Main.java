144eeaca448165bfd590b8e788a6d2534ce5dfb0
Valentin <qweqweq@qwe>
2021-06-27 13:44:29 +0300

    Правки замечаний

--- src/main/java/com/vvvxd/File_CRUD_Java/Main.java
+++ src/main/java/com/vvvxd/File_CRUD_Java/Main.java
@@ -9,16 +9,12 @@ public class Main {
     public static void main(String[] args) {
         Skill sk = new Skill( 100502L,"Java");
         SkillView sr = new SkillView();
-//        sr.getAll();
-//        sr.save();
+//       sr.getAll();
+      //  sr.save();
 //          sr.update(100501L);
 //        sr.getById(100301L);
 
-        sr.deleteById(100301L);
-//        List<Skill> a = sr.getAll();
-//        System.out.println(a.toString());
+//        sr.deleteById(100301L);
 
-//        sr.deleteById(100501L);
-//        System.out.println(sr.getAll().toString());
     }
 }
--- src/main/java/com/vvvxd/File_CRUD_Java/view/SkillView.java
+++ src/main/java/com/vvvxd/File_CRUD_Java/view/SkillView.java
@@ -3,33 +3,27 @@ package com.vvvxd.File_CRUD_Java.view;
 import com.vvvxd.File_CRUD_Java.controller.SkillController;
 import com.vvvxd.File_CRUD_Java.model.Skill;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Scanner;
 
 public class SkillView {
     private final SkillController skillController = new SkillController();
-    Skill skill;
-    List<Skill> skillList;
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    String data;
+    Scanner scanner = new Scanner(System.in);
 
     public void save() {
         System.out.println("Enter the name of the skill: ");
-        try {
-            data = reader.readLine();
-            if (!data.isEmpty()) {
-                skill = skillController.save(data);
-                System.out.println(skill.toString());
-            }
-        } catch (IOException e) {
-            System.out.println("Enter the correct name.");
+        Skill skill;
+        String data;
+        data = scanner.nextLine();
+        if (!data.isEmpty()) {
+            skill = skillController.save(data);
+            System.out.println(skill.toString());
         }
     }
 
     public void getAll() {
         try {
+            List<Skill> skillList;
             skillList = skillController.getAll();
             if (skillList.isEmpty()) {
                 return;
@@ -45,6 +39,8 @@ public class SkillView {
 
     public void getById(Long id) {
         try {
+            List<Skill> skillList;
+            Skill skill;
             skillList = skillController.getAll();
             if (skillList.isEmpty()) {
                 return;
@@ -58,22 +54,24 @@ public class SkillView {
 
     public void update(Long id) {
         try {
+            Skill skill;
             System.out.println("Write new name of skill: ");
-            skill = skillController.update(new Skill(id,reader.readLine()));
+            skill = skillController.update(new Skill(id, scanner.nextLine()));
             System.out.println(skill.toString());
-        } catch (IOException | NumberFormatException exception) {
+        } catch (NumberFormatException exception) {
             System.out.println("Wrong id or name.");
         }
     }
 
     public void deleteById(Long id) {
         try {
+            List<Skill> skillList;
             skillList = skillController.getAll();
             if (skillList.isEmpty()) {
                 return;
             }
             skillController.deleteById(id);
-        } catch (  NumberFormatException e) {
+        } catch (NumberFormatException e) {
             System.out.println("Write correct id.");
         }
     }
